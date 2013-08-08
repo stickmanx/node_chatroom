b1720b0abdcace79279a04b18fea6211f80fa68c
stickmanx <stickmanx@gmail.com>
2013-08-08 07:14:43 -0700

    create server side socket io server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var mime = require('mime');
 var cache = {};
 
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
+
 // Helper Functions
 // 1. Send 404 errors when file requested doesn't exist
 function send404(response) {
